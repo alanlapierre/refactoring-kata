8a5c3886fe28b74a7d237ed9209cbd6a52413b2f
Alan Lapierre <alan.lapierre@mercadolibre.com>
2020-07-08 08:59:52 -0300

    Mover método
    Se mueve método calcularImporte de la clase Pelicula a la clase PrecioPelicula.

--- src/videoclub/Pelicula.java
+++ src/videoclub/Pelicula.java
@@ -44,27 +44,7 @@ public class Pelicula {
 	}
 
 	public double calcularImporte(Integer diasRentada) {
-		Double monto = 0D;
-		switch (getTipo()) {
-			case Pelicula.CATALOGO:
-				monto += 2;
-				if (diasRentada > 2) {
-					monto += (diasRentada - 2) * 1.5;
-				}
-				break;
-			case Pelicula.ESTRENO:
-				monto += diasRentada * 3;
-				break;
-			case Pelicula.INFANTIL:
-				monto += 1.5;
-				if (diasRentada > 3) {
-					monto += (diasRentada - 3) * 1.5;
-				}
-				break;
-			default:
-				break;
-		}
-		return monto;
+		return precio.calcularImporte(diasRentada);
 	}
 
 	public int calcularPuntosClienteFrecuente(Integer diasRentada) {
--- src/videoclub/PrecioPelicula.java
+++ src/videoclub/PrecioPelicula.java
@@ -4,5 +4,29 @@ public abstract class PrecioPelicula {
 
     public abstract Integer getTipoPelicula();
 
+    public double calcularImporte(Integer diasRentada) {
+        Double monto = 0D;
+        switch (getTipoPelicula()) {
+            case Pelicula.CATALOGO:
+                monto += 2;
+                if (diasRentada > 2) {
+                    monto += (diasRentada - 2) * 1.5;
+                }
+                break;
+            case Pelicula.ESTRENO:
+                monto += diasRentada * 3;
+                break;
+            case Pelicula.INFANTIL:
+                monto += 1.5;
+                if (diasRentada > 3) {
+                    monto += (diasRentada - 3) * 1.5;
+                }
+                break;
+            default:
+                break;
+        }
+        return monto;
+    }
+
 }
 
